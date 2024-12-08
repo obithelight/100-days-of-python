1f3347ac9c3431fdcd71b6bb9b86527cd512a9da
Obi Chukwu <kennetho.onu@gmail.com>
2024-12-08 05:36:23 +0100

    band generator

---
+++ beginner/day-01/main.py
@@ -0,0 +1,11 @@
+# Create a greeting for your band name generator program
+print("Welcome to the Band Name Generator.")
+
+# Ask for the name of city user grew up in, followed by a new line
+city = input("What's the name of the city you grew up in? \n")
+
+# Ask for the name of the user's pet, followed by a new line
+pet = input("What's your pet's name? \n")
+
+# Print the band name as a combination of the user's city and pet name
+print(f"Your band name could be {city} {pet}")
