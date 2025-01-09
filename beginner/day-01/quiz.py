003fdfd796df28fff30eb9946903cd374c4c7f56
Obi Chukwu Onu <kennetho.onu@gmail.com>
2025-01-09 16:07:41 +0100

    day_01 learning quiz

---
+++ beginner/day-01/quiz.py
@@ -0,0 +1,47 @@
+'''
+Variable Naming Quiz
+'''
+
+# Question 1: Which line of Python code is valid?
+
+'''
+a) var a = 12
+b) a = 12
+c) a: 12
+d) 12 = a
+
+**answer** 
+b) a = 12
+'''
+
+# Question 2: Which is the best variable name for Player 1's username?
+
+'''
+a) p1 user name = "jackbauer"
+b) 1_player_username = "jackbauer"
+c) player1_username = "jackbauer"
+d) p1u = "jackbauer"
+
+**answer**
+c) palyer1_username = "jackbauer"
+'''
+
+# Question 3: Which block of code will produce an error? For extra points, which type of error do you think it will produce?
+
+'''
+a) 1 | time_until_midnight = "5"
+   2 | print("There are " + time_until_Midnight + " hours until midnight")
+
+b) 1 | num_hours = "5"
+   2 | print("There are " + num_hours + " hours until midnight")
+
+c) 1 | time_until_midnight = "5"
+   2 | print("There are "+time_until_Midnight+" hours until midnight")
+
+**answer**
+a) 1 | time_until_midnight = "5"
+   2 | print("There are " + time_until_Midnight + " hours until midnight")
+
+**bonus**
+It will produce a NameError: time_until_Midnight
+'''
