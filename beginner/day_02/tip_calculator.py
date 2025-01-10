ae8ab86749a7cdc2e4ccae3db4ba043fa25f01f0
Obi Chukwu Onu <kennetho.onu@gmail.com>
2025-01-10 17:39:54 +0100

    build a tip calculator

--- beginner/day_02/tip_calculator.py
+++ beginner/day_02/tip_calculator.py
@@ -1,24 +1,36 @@
-# Create a program that calculates a bill split between friends and adds a percentage tip to the it
+# Tip Calculator Project
+'''
+Create a program that calculates a bill split between friends and adds a percentage tip to the bill.
+
+Example:
+If the bill was $150.00, split between 5 people, with 12% tip.
+
+Each person should pay:
+
+(150.00 / 5) * 1.12 = 33.6
+
+After formatting the result to 2 decimal places = 33.60
+'''
 
 # Print a welcome message
 print("Welcome to the tip calculator.")
 
 bill = float(input("What was the total bill? $"))
 
-tip = int(input("What percentage tip would you like to give? 10, 12, 0r 15? "))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 
-people = int(input("How many people to split the bill? "))
+no_of_persons = int(input("How many people to split the bill? "))
 
 # Get tip amount using the percentage tip * bill
-tip_amount = bill * (tip / 100)
+tip_amount = (tip / 100) * bill
 
 # Add tip_amount to bill
-total_bill = tip_amount + bill
+final_bill = tip_amount + bill
 
-# Divide total bill by number of people to get what each person will pay
-each_to_pay = total_bill / people
+# Divide final_bill by no_of_persons to get what each person will pay
+each_to_pay = final_bill / no_of_persons
 
-# Round up each_to_pay to 2 decimal numbers
+# Round up each_to_pay to 2 decimal numbers as per requirements
 each_to_pay = round(each_to_pay, 2)
 
 # Print the output message with final_bill for each person
