87715410970a11ee13cd377403ba3755de94439a
Obi Chukwu Onu <kennetho.onu@gmail.com>
2025-01-10 14:13:08 +0100

    type error, checking, and conversion

--- beginner/day_02/tasks/task_2.py
+++ beginner/day_02/tasks/task_2.py
@@ -33,6 +33,6 @@ int()
 str()
 '''
 # 3. Make this line of code run without errors
-# print("Number of letters in your name: " + len(input("Enter your name")))
+# print("Number of letters in your name: " + len(input("Enter your name: ")))
 
 print("Number of letters in your name: " + str(len(input("Enter your name: "))))
