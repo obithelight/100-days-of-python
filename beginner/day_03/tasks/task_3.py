036f829b951b6084619b7c0222697747ca97e822
Obi Chukwu Onu <kennetho.onu@gmail.com>
2025-01-11 02:27:41 +0100

    math and english scores

--- beginner/day_03/tasks/task_3.py
+++ beginner/day_03/tasks/task_3.py
@@ -2,16 +2,18 @@
 '''
 You can put if/else statements inside other if/else statements. This is known as nesting. e.g.
 '''
-math_score = int(input("What did you score in math? "))
+maths_score = int(input("What did you score in math? "))
 english_score = int(input("What did you score in english? "))
 
 if maths_score >= 90:
     if english_score >= 90:
-        print("You're good at everything")
+        print("You're good at both english and maths.")
     else:
-        print("You're good at maths")
-if english_score >= 90:
-    print("You're good at english)
+        print("You're good at maths.")
+elif english_score >= 90:
+    print("You're good at english.")
+else:
+    print("Work hard to improve your english and math scores.")
 '''
 In this case only when a student has over 90 on maths and english, do they get "You are good at everything".
 '''
