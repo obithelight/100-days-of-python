f40701c4dbd9c69817ea0e4b5ed4c82d75c74b4b
Obi Chukwu Onu <kennetho.onu@gmail.com>
2025-01-18 01:35:02 +0100

    removed extra spaces/lines in code

--- beginner/day_03/treasure_island/main.py
+++ beginner/day_03/treasure_island/main.py
@@ -1,7 +1,6 @@
 import time
 from leftRight import left_or_right
 
-
 print("Welcome to Treasure Island.\nYour mission is to find the missing treasure.")
 time.sleep(3)
 print("---------------------------------------------")
