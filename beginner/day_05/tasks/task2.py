fa0de455b67ee9c09ca832216b4d69c1ec9e9dcd
Obi Chukwu Onu <kennetho.onu@gmail.com>
2025-01-18 13:58:20 +0100

    fizzbuzz challenge

---
+++ beginner/day_05/tasks/task2.py
@@ -0,0 +1,10 @@
+# The Gauss Challenge
+'''
+Work out the total of the numbers between 1 and 100, inclusive of both 1 and 100.
+'''
+total = 0
+for number in range(1, 101):
+    total += number
+
+print(total)
+
