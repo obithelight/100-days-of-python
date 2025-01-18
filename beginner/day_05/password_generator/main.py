198211084e017f686ef2db60e4080ea645c71d68
Obi Chukwu Onu <kennetho.onu@gmail.com>
2025-01-18 17:08:41 +0100

    password generator program

--- beginner/day_05/password_generator/main.py
+++ beginner/day_05/password_generator/main.py
@@ -1,37 +1,67 @@
 import random
+import time
+import os
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
+time.sleep(1)
 
-myletters = ''
-mynumbers = ''
-mysymbols = ''
+while True:
+    nr_letters = input("How many letters should be present in your password? ")
+    if not nr_letters.isdigit():
+        os.system('clear')
+        print("Wrong input. Please enter a number.")
+    else:
+        break
+    
+while True:
+    nr_numbers = input("How many numbers should be present in your password? ")
+    if not nr_numbers.isdigit():
+        os.system('clear')
+        print("Wrong input. Please enter a number.")
+    else:
+        break
+
+while True:
+    nr_symbols = input("How many symbols should be present in your password? ")
+    if not nr_symbols.isdigit():
+        os.system('clear')
+        print("Wrong Input. Please enter a number.")
+    else:
+        break
+
+nr_letters = int(nr_letters)
+nr_numbers = int(nr_numbers)
+nr_symbols = int(nr_symbols)
+
+mypassword = ''
 
 for _ in range(nr_letters):
-    myletters += random.choice(letters)
-# print(myletters)
+    mypassword += random.choice(letters)
+# print(mypassword)
 
 for _ in range(nr_numbers):
-    mynumbers += random.choice(numbers)
-# print(mynumbers)
+    mypassword += random.choice(numbers)
+# print(mypassword)
 
 for _ in range(nr_symbols):
-    mysymbols += random.choice(symbols)
-# print(mysymbols)
+    mypassword += random.choice(symbols)
+# print(mypassword)
 
-# Easy Level: print the password sequentially
-password = myletters + mynumbers + mysymbols
-print("Easy Level:", password)
+
+# Easy Level: Print the password sequentially
+os.system('clear')
+print("Easy Level Password:", mypassword)
+
 
 # Hard Level: randomly rearrange the password 
-password = list(password)
-random.shuffle(password)
+# step 1: convert string to list to shuffle
+mypassword = list(mypassword)
+random.shuffle(mypassword)
 
-password = "".join(password)
-print("Hard Level:", password)
+# step 2: convert list back to string to concatenate
+mypassword = "".join(mypassword)
+print("Hard Level Password:", mypassword)
