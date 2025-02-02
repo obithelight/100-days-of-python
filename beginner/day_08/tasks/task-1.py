938c46b66c9c2719cf2c318320d7b14a3c628e07
Obi Chukwu Onu <kennetho.onu@gmail.com>
2025-02-02 14:11:19 +0100

    printing inside a function

---
+++ beginner/day_08/tasks/task-1.py
@@ -0,0 +1,12 @@
+'''
+Create a function called greet().
+Write 3 print statements inside the function.
+Call the greet() function and run your code.
+'''
+
+def greet():
+    print("Hello")
+    print("February")
+    print(2025)
+
+greet()
